239861dd3f46653d0607bfb73fd38a411d5d8a51
Andreé Sandoval Rojas <andree.bit@gmail.com>
2017-04-25 07:04:32 -0500

    adding EventTypeController and ServiceCategoryController

---
+++ app/Http/Controllers/Api/V1/EventTypeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Api\Controller;
+
+class EventTypeController extends Controller
+{
+
+    public function index() {
+
+    }
+
+}
